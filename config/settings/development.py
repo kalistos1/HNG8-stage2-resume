6b229ea6585729c5dfe8a64cb1ba5a3625989e97
kalistos1 <ucktech1@gmail.com>
2021-08-19 13:49:16 +0100

    rerolled settings.py

--- config/settings/development.py
+++ config/settings/development.py
@@ -6,6 +6,7 @@ from . base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
--- resume/settings.py
+++ resume/settings.py
@@ -10,7 +10,7 @@ from django.contrib import messages
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -21,6 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
