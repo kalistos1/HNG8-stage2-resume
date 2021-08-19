6e3d36f644fcc86f558720a2d6a6e53125db23bb
kalistos1 <ucktech1@gmail.com>
2021-08-19 14:33:27 +0100

    modifies static root

--- resume/settings.py
+++ resume/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
