8d70026a66b21baa256fe223c2e6ef65b66737de
kalistos1 <ucktech1@gmail.com>
2021-08-19 14:04:21 +0100

    removed decouple config

--- resume/settings.py
+++ resume/settings.py
@@ -139,8 +139,8 @@ MESSAGE_TAGS ={
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'ucktech1@gmail.com'
+EMAIL_HOST_PASSWORD = 'wyxzbknmmqemokft'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
